22f6625c0af1d08dfa16d15682acbc431ba507cd
zhangting <18838963547@163.com>
2022-09-07 22:48:11 +0800

    docs(引入ui组件库): antd

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@
 // export default App;
 
 import React,{Component} from "react";
+import { Button } from "antd";
+import 'antd/dist/antd.css'
 
 import './App.css';
 // import Hello from "./components/Hello";
@@ -26,6 +28,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        <Button type='primary'>按钮</Button>
         <RouterCom />
         {/* <GitSearch /> */}
         {/* <RequestCom /> */}
